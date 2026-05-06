6f11a6d7d42528c2eb9d2d17a261f321a28cbc72
khalilSediq <khalilullahsadique1334@gmail.com>
2026-05-06 15:09:19 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+
+
+const App = () => {
+
+  const [todos, setTodos] = useState([])
+  const [ title , setTitle] = useState("")
+  const [description , setDescription] = useState("")
+
+    const addTodoTo = ( e ) =>{
+      e.preventDefault()
+      console.log(title, description);
+
+      setTodos([...todos, {title, description}])
+      
+
+
+    }
+  return (
+    <>
+    <form onSubmit={addTodoTo} >
+      <input onChange={(e)=> setTitle(e.target.value)} value={title} type="text" placeholder='Enter Title'  />
+      <input onChange={(e) => setDescription(e.target.value)} value={description} type="text" placeholder='Enter Description '  />
+      <button type='submit'>Add todo </button>
+    </form>
+
+
+    {todos.length > 0 ? todos.map((item, index) =>{
+      return <div key={index}>
+        <h1>{item.title}</h1>
+        <h4> {item.description} </h4>
+      </div>
+    } ) : <h2>No todos found</h2> }
+    </>
+  )
+}
+
+export default App
