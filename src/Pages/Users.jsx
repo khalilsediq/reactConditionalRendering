e9caac6ddeeed04aac9a1ee4e56942cc0ba80292
khalilSediq <khalilullahsadique1334@gmail.com>
2026-05-08 10:39:44 +0500

    Refactor Users component to improve user fetching logic and error handling; add fetchUsers function and update loading/error states

--- src/Pages/Users.jsx
+++ src/Pages/Users.jsx
@@ -5,30 +5,66 @@ const Users = () => {
 
     const [users, SetUsers] = useState(null)
     const [Load, setLoad] = useState(true)
-    const [error, setEroor ] = useState(false)
+    const [error, setError] = useState(false)
 
+    // const loadUsers = async () => {
+    //     try{
+    //          let res = await fetch("https://jsonplaceholder.typicode.com/users")
+    //         let data = await res.json()
+    //         SetUsers(data)
+    //         console.log(data);
+    //     } catch (err){
+    //         console.log(err);
+    //         setError(true)
 
-    useEffect(()=>{
-
-        fetch("https://jsonplaceholder.typicode.com/users")
-        .then(res => res.json())
-        .then(res => {
-            console.log(res);
-            SetUsers(res)
             
-        })
-        .catch(error =>{
-            console.log(error);
-            
-            setEroor(true)
-        } )
-        .finally(()=> {
+    //     } finally {
+    //         setLoad(false)
+    //     }
+    // }
 
+    // useEffect(()=>{
+    //     loadUsers()
+    //     // fetch("https://jsonplaceholder.typicode.com/users")
+    //     // .then(res => res.json())
+    //     // .then(res => {
+    //     //     console.log(res);
+    //     //     SetUsers(res)
+            
+    //     // })
+    //     // .catch(error =>{
+    //     //     console.log(error);
+            
+    //     //     setEroor(true)
+    //     // } )
+    //     // .finally(()=> {
+
+    //     //     setLoad(false)
+    //     // } )
+
+    // },  [])
+
+    const fetchUsers = async () => {
+        try {
+            let response = await fetch("https://jsonplaceholder.typicode.com/users")
+            let data = await response.json()
+            SetUsers(data)
+            console.log(data);
+            
+        } catch (err){
+            console.log(err);
+            setError(true)
+                       
+        } finally {
             setLoad(false)
-        } )
+        }
+    }
 
-    },  [])
-
+    useEffect(() => {
+      fetchUsers()
+    }, [])
+    
+    
   return (
     <>
     <div style={{
@@ -37,11 +73,11 @@ const Users = () => {
         justifyContent: 'space-evenly',
     }}>
         
-    {Load && <h2> Loading Please wait</h2>}
-    {error && <h2> Error Occured on over end, please try again later</h2>}
-    {users && users.map((item => {
+   {Load && <h2>Loading Please wait</h2> }
+    {error && <h2> Error occurred on our end, please try again later</h2>}
+    {users && users.map((item) => {
         return <Card title={item.name} email={item.email} phone={item.phone} key={item.id} />
-    }))}
+    })}
     </div>
     </>
   )
